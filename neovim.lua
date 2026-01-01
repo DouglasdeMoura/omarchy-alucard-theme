f5bacf9e3e3173ef2a046d7a2f5b0447ce312d43
Douglas Moura <douglas.ademoura@gmail.com>
2026-01-01 16:32:45 -0300

    💄 Add Alucard themes

---
+++ neovim.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"jaljoue/dracula-alucard.nvim",
+		opts = {
+			transparent_bg = true,
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "dracula-alucard",
+		},
+	},
+}
